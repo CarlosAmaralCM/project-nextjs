8b72065e423bc56e375597e541d6146462868794
CarlosAmaralCM <carlos.amaral.dev@outlook.com>
2024-12-11 13:00:26 -0400

    [FEAT] Criação de componente inicial

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+        <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I'm Carlos</h1>
+        <p className="text-white text-lg lg:text-xl font-mono">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, dolorum!
+        </p>
+    </section>
+  )
+}
+
+export default HeroSection
