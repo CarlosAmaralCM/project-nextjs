751daeccdf6a96acdfdd98d484f216b32419e0e4
CarlosAmaralCM <carlos.amaral.dev@outlook.com>
2024-12-12 12:33:56 -0400

    [Feat] Adiciona novas estilizações e  botoes no component HeroSection

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
-
+import Image from 'next/image'
 const HeroSection = () => {
   return (
     <section>
-        <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I'm Carlos</h1>
-        <p className="text-white text-lg lg:text-xl font-mono">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, dolorum!
-        </p>
+      <div className='grid grid-cols-1 sm:grid-cols-12'>
+        <div className='col-span-7 place-self-center text-center sm:text-left'>
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            Hello, I'm Carlos
+          </h1>
+          <p className="text-white text-base sm:text-lg mb-6 lg:text-xl font-mono">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, dolorum!
+          </p>
+          <div>
+            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-gray-500 hover:bg-slate-200 text-black'>Hire me</button>
+            <button className='px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>Download CV</button>
+          </div>
+        </div>
+        <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+          <div className='rounded-full bg-[#181818] w-[450px] h-[450px] lg:w-[400px] lg:h-[400px] relative'>
+            <Image
+              src="/img/hero.png"
+              alt='hero image'
+              className='rounded-3xl absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
